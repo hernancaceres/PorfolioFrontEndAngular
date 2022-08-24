4c98929da8f88d3108fad7a37e75cc6bff67aaf1
hernancaceres <harinatha777@hotmail.com>
2022-08-24 08:42:49 -0300

    acerca-de

--- src/app/public/public.module.ts
+++ src/app/public/public.module.ts
@@ -1,16 +1,29 @@
 import { NgModule } from '@angular/core';
 import { SharedModule } from '../core/shared/shared.module';
+import { PublicRoutingModule } from './public-routing.module';
 
 import { HomeComponent } from './home/containers/home.componet';
+import { HeaderComponent } from './header/containers/header.component';
 import { LoginComponent } from './login/containers/login.component';
-import { PublicRoutingModule } from './public-routing.module';
+import { AplogoComponent } from './header/components/aplogo/aplogo.component';
 import { PublicComponent } from './public.component';
+import { SocialComponent } from './header/components/social/social.component';
+import { BannerComponent } from './header/components/banner/banner.component';
+import { AcercaDeComponent } from './acerca-de/containers/acerca-de/acerca-de.component';
+
+
 
 @NgModule({
   declarations: [
     HomeComponent,
     LoginComponent,
-    PublicComponent
+    HeaderComponent,
+    AplogoComponent,
+    PublicComponent,
+    SocialComponent,
+    BannerComponent,
+    AcercaDeComponent,
+    
   ],
   imports: [
     PublicRoutingModule,
